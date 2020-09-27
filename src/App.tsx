1bfa0d9f3b27a9a0a3822b4ce45724725b4f51b6
Amaterasus <skyetay03@googlemail.com>
2020-09-27 19:59:54 +0100

    home added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch } from "react-router-dom";
 import './App.css';
+import Home from './Components/Home';
 import NavBar from './Components/NavBar';
 import AboutMe from './Components/AboutMe';
 import Footer from './Components/Footer';
@@ -10,6 +11,9 @@ const App = () => {
     <div className="App">
       <NavBar />
       <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
         <Route exact path="/aboutme">
           <AboutMe />
         </Route>
---
+++ src/Components/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+      <div>
+          Welcome to my Portfolio
+      </div>
+  );
+}
+
+export default NavBar;
