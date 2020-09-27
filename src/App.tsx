17229d87dbd15c4f62ec233cd968e319ecf8ec8b
Amaterasus <skyetay03@googlemail.com>
2020-09-27 19:39:25 +0100

    footer made

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import AboutMe from './Components/AboutMe';
+import Footer from './Components/Footer';
 
 const App = () => {
   return (
     <div className="App">
       <AboutMe />
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+        Contact me at Skyetay03@googlemail.com
+    </footer>
+  );
+}
+
+export default Footer;
