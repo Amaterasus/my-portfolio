4453500fd6979f86d8082a51665447647defd252
Amaterasus <skyetay03@googlemail.com>
2020-09-27 19:54:06 +0100

    navbar created

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import NavBar from './Components/NavBar';
 import AboutMe from './Components/AboutMe';
 import Footer from './Components/Footer';
 
 const App = () => {
   return (
     <div className="App">
+      <NavBar />
       <AboutMe />
       <Footer />
     </div>
